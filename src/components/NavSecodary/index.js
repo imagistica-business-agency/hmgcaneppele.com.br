d14ca01b4350a6532d2a6155b4212ff2c431d18e
Daniel Soares <daniel.soares@me.com>
2020-07-23 11:10:43 -0300

    adding NavSecondary

---
+++ src/components/NavSecodary/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import * as S from './styled'
+import links from './links'
+
+const NavSecondary = () => (
+  <S.NavWrapper>
+    <S.NavList>
+      {links.map((link, i) => (
+        <S.NavItem key={i}>
+          <S.NavItemLink to={link.url} activeClassName="active">
+            {link.label}
+          </S.NavItemLink>
+        </S.NavItem>
+      ))}
+    </S.NavList>
+  </S.NavWrapper>
+)
+
+export default NavSecondary
---
+++ src/components/NavSecodary/links.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Estruturas em aço',
+    url: '/servicos#estruturas-aco'
+  },
+  {
+    label: 'Painéis térmicos',
+    url: '/servicos#paines-termicos'
+  },
+  {
+    label: 'Telhas térmicas',
+    url: '/servicos#telhastermicas'
+  },
+  {
+    label: 'Telhas metálicas',
+    url: '/servicos#telhasmetalicas'
+  },
+  {
+    label: 'Construção isotérmica',
+    url: '/servicos#construcao-isotermica'
+  }
+]
+
+export default links
