f478264aa2eb6f58cd8fc407af3a0860bdad7bf4
Daniel Soares <daniel.soares@me.com>
2020-07-23 11:10:22 -0300

    adding Nav

---
+++ src/components/Nav/links.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Início',
+    url: '/'
+  },
+  {
+    label: 'Quem somos',
+    url: '/quem-somos/'
+  },
+  {
+    label: 'Serviços',
+    url: '/servicos/'
+  },
+  {
+    label: 'Obras realizadas',
+    url: '/obras-realizadas/'
+  },
+  {
+    label: 'Contatos',
+    url: '/contatos/'
+  }
+]
+
+export default links
---
+++ src/components/Nav/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+export const NavWrapper = styled.nav``
+
+export const NavList = styled.ul`
+  list-style: none;
+  margin: 0;
+`
+
+export const NavItem = styled.li``
+
+export const NavItemLink = styled(Link)`
+  text-transform: uppercase;
+`
