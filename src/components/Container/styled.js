4c6e5e0909ca4135f8f202892fb4c9e13d03ef93
Daniel Soares <daniel.soares@me.com>
2020-07-23 11:10:01 -0300

    adding Container

---
+++ src/components/Container/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  margin: 0 auto;
+  max-width: 960px;
+`
+
+export const ContainerLarge = styled.main`
+  margin: 0 auto;
+  max-width: 1260px;
+  padding: 1.45rem 1.0875rem;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+import { Container } from '../components/Container/styled'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Container>
       <h1>Hi people</h1>
+    </Container>
   </Layout>
 )
 
