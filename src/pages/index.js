d0a5350303afefa6c9fec74e0191a5a12fb92aaa
Daniel Soares <daniel.soares@me.com>
2020-07-28 13:11:38 -0300

    adding Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+import Hero from '../components/Hero'
 import Objetivo from '../components/Objetivo'
 import Projetos from '../components/Projetos'
 import Diferencial from '../components/Diferencial'
@@ -11,6 +12,7 @@ import ContactBanner from '../components/ContactBanner'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Hero />
     <Objetivo />
     <Projetos />
     <Diferencial />
